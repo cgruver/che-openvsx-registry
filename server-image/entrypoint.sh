0243b1801e1bf312e75d684d9dc445d1079a9748
Charro Gruver <cgruver@clg.lab>
2023-05-10 14:11:01 -0400

    wip

--- server-image/entrypoint.sh
+++ server-image/entrypoint.sh
@@ -9,4 +9,6 @@ pushd /openvsx-server || return
 printf "Waiting until openvsx is ready"
 timeout 0 bash -c "until curl --output /dev/null --head --silent --fail http://localhost:8080/user; do printf '.'; sleep 1; done"
 printf "Openvsx is ready"
+popd
+node /webui/lib/default/server &
 tail -f /dev/null
